0f20348352b9649ef531018d1e01ed991534c157
AdrieanKhisbe <adriean.khisbe@live.fr>
2020-12-26 22:55:59 +0100

    Set up yargs and minimal help :pirate_flag:

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const {Transform} = require('stream');
 const pump = require('pump');
 const emojiIndex = require('gemoji/name-to-emoji.json');
+const yargs = require('yargs/yargs');
 
 const EMOJI_REGEX = /(:[^\s:]+:)/g;
+
+const parseArgs = argz =>
+  yargs(argz)
+    .usage(
+      'Replace your emoji codes.\n\nUsage: cat something | $0\n       $0 "text with :slightly_smiling_face:"'
+    )
+    .alias('h', 'help').argv;
 
 const splitAndReplaceEmojies = (string, accumulator = null) => {
   const parts = string.split(EMOJI_REGEX);
@@ -32,6 +40,8 @@ const getEmojizerStream = () =>
 const replaceEmojiCodes = string => splitAndReplaceEmojies(string).join('');
 
 const main = (input = process.stdin, output = process.stdout) => {
+  // eslint-disable-next-line no-unused-vars
+  const args = parseArgs(process.argv.slice(2));
   input.setEncoding('utf8');
   output.setEncoding('utf8');
   pump(input, getEmojizerStream(), output);
