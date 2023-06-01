7a5e0f33997fa9cd29698dba2e19e54370f409a2
AdrieanKhisbe <adriean.khisbe@live.fr>
2023-06-01 10:18:44 +0200

    Bump ava and eslint (and necessary tweaks) :rocket:
    callback based tests as to be migrated and ava import/no-unresolved eslint error mute

--- test/emojizer.integration.test.js
+++ test/emojizer.integration.test.js
@@ -1,69 +1,51 @@
 import path from 'path';
 import {fileURLToPath} from 'url';
 import childProcess from 'child_process';
+import {promisify} from 'util';
 import test from 'ava';
+
+const exec = promisify(childProcess.exec);
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const EMOJIIZER_CLI = path.join(path.dirname(__dirname), 'bin', 'emojizer.js');
 
-test.cb('emojizer cli stdin', t => {
-  childProcess.exec(
-    `echo "shell is here :ocean: :shell:" | ${EMOJIIZER_CLI}`,
-    (err, stdout, stderr) => {
-      t.is(err, null);
-      t.is(stderr.trim(), '');
-      t.is(stdout.trim(), 'shell is here 🌊 🐚');
-      t.end();
-    }
-  );
+test('emojizer cli stdin', async t => {
+  const {stdout, stderr} = await exec(`echo "shell is here :ocean: :shell:" | ${EMOJIIZER_CLI}`);
+  t.is(stderr.trim(), '');
+  t.is(stdout.trim(), 'shell is here 🌊 🐚');
 });
 
-test.cb('emojizer cli provided values', t => {
-  childProcess.exec(`${EMOJIIZER_CLI} "lol :+1:" ":-1:"`, (err, stdout, stderr) => {
-    t.is(err, null);
-    t.is(stderr.trim(), '');
-    t.is(stdout.trim(), 'lol 👍\n👎');
-    t.end();
-  });
+test('emojizer cli provided values', async t => {
+  const {stdout, stderr} = await exec(`${EMOJIIZER_CLI} "lol :+1:" ":-1:"`);
+
+  t.is(stderr.trim(), '');
+  t.is(stdout.trim(), 'lol 👍\n👎');
 });
 
-test.cb('emojizer cli with single file', t => {
-  childProcess.exec(
-    `${EMOJIIZER_CLI} -f data/test-one.txt`,
-    {cwd: __dirname},
-    (err, stdout, stderr) => {
-      t.is(err, null);
-      t.is(stderr.trim(), '');
-      t.is(stdout.trim(), 'First 🥇');
-      t.end();
-    }
-  );
+test('emojizer cli with single file', async t => {
+  const {stdout, stderr} = await exec(`${EMOJIIZER_CLI} -f data/test-one.txt`, {cwd: __dirname});
+  t.is(stderr.trim(), '');
+  t.is(stdout.trim(), 'First 🥇');
 });
 
-test.cb('emojizer cli with missing file', t => {
-  childProcess.exec(
-    `${EMOJIIZER_CLI} -f data/test-AWOL.txt`,
-    {cwd: __dirname},
-    (err, stdout, stderr) => {
-      t.not(err, null);
-      t.is(err.killed, false);
-      t.is(err.code, 1);
-      t.is(stderr.trim(), "Provided file path does not exists: 'data/test-AWOL.txt'");
-      t.is(stdout.trim(), '');
-      t.end();
-    }
-  );
+test('emojizer cli with missing file', async t => {
+  try {
+    await exec(`${EMOJIIZER_CLI} -f data/test-AWOL.txt`, {cwd: __dirname});
+  } catch (err) {
+    t.not(err, null);
+    t.is(err.killed, false);
+    t.is(err.code, 1);
+    t.is(err.stderr.trim(), "Provided file path does not exists: 'data/test-AWOL.txt'");
+    t.is(err.stdout.trim(), '');
+  }
 });
 
-test.cb('emojizer cli with file list', t => {
-  childProcess.exec(
+test('emojizer cli with file list', async t => {
+  const {stdout, stderr} = await exec(
     `${EMOJIIZER_CLI} -f data/test-one.txt data/test-two.txt data/test-three.txt`,
-    {cwd: __dirname},
-    (err, stdout, stderr) => {
-      t.is(err, null);
-      t.is(stderr.trim(), '');
-      t.is(stdout.trim(), 'First 🥇\nSecond 🥈\nThird 🥉');
-      t.end();
-    }
+    {cwd: __dirname}
   );
+
+  t.is(stderr.trim(), '');
+  t.is(stdout.trim(), 'First 🥇\nSecond 🥈\nThird 🥉');
 });
